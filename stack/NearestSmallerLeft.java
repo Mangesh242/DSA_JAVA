ab3d094d562c940772d0be7d278e70dbfc6b0fe1
mangesh242 <mangeshshinde242@gmail.com>
2023-12-21 17:51:28 +0530

    NSELI as per index

--- stack/NearestSmallerLeft.java
+++ stack/NearestSmallerLeft.java
@@ -8,6 +8,30 @@ public class NearestSmallerLeft {
         int[] arr={8,2,4,9,7,6,3,10};
 
         System.out.println(Arrays.toString(NSEL(arr)));
+        System.out.println(Arrays.toString(NSELI(arr)));
+    }
+
+    static int[] NSELI(int[] arr){
+        Stack<Integer> st=new Stack<>();
+        int[] res=new int[arr.length];
+
+        for(int i=0;i<arr.length;i++){
+
+            while(!st.isEmpty() && arr[i] <=arr[st.peek()]){
+                st.pop();
+            }
+            if(!st.isEmpty())
+            {
+                res[i]=st.peek();
+            }else{
+                res[i]=-1;
+            }
+            st.push(i);
+        }
+
+        return res;
+
+        
     }
 
     static int[] NSEL(int[] arr){
