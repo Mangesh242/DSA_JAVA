5f7dd77380533d1586dd6725e740c626b61fd088
mangesh242 <mangeshshinde242@gmail.com>
2023-12-24 22:14:49 +0530

    add form binary tree, added inorder, preorder and postorder traversal

--- LinkedList/ReverseLL.java
+++ LinkedList/ReverseLL.java
@@ -26,13 +26,13 @@ public class ReverseLL {
     public static void main(String[] args) {
         ListNode head=null;
         ListNode nn=new ListNode(2);
-        head=nn;
         ListNode nn2=new ListNode(3);
         ListNode nn3=new ListNode(4);
+        head=nn;
         head.next=nn2;
         head.next.next=nn3;
 
-            Solution obj=new Solution();
+        Solution obj=new Solution();
         ListNode ans=obj.reverseList(head);
         ListNode temp=ans;
         while(temp!=null){
--- tree/FormBinaryTree.java
+++ tree/FormBinaryTree.java
@@ -15,10 +15,37 @@ public class FormBinaryTree {
     }
 
     public static void main(String[] args) {
-        // FormBinaryTree x=new FormBinaryTree();
-        // TreeNode node=x.new TreeNode("+");
+        FormBinaryTree x=new FormBinaryTree();
+        TreeNode node=x.new TreeNode("+");
+        TreeNode node2=x.new TreeNode("a");
+        TreeNode node3=x.new TreeNode("b");
+        node.left=node2;
+        node.right=node3;
+        x.printInOrder(node);
+        x.printPreOrder(node);
+        x.printPostOrder(node);
+    }
 
+    public void printInOrder(TreeNode node){
+        if(node==null)return;
+        printInOrder(node.left);
+        System.out.println(node.val);
+        printInOrder(node.right);
+    }
+    
+    public void printPostOrder(TreeNode node){
+        if(node==null)return;
+        printInOrder(node.left);
+        printInOrder(node.right);
+        System.out.println(node.val);
+    }
 
+    public void printPreOrder(TreeNode node){
+        if(node==null)return;
+        System.out.println(node.val);
+        printInOrder(node.left);
+        printInOrder(node.right);
+        
     }
     
 }
